040fe0360a397fcb61d7e9acb98d3b24fd6c2cae
johnmichael <91875751+michaelll22@users.noreply.github.com>
2025-09-04 14:25:33 +0800

    feat: add buttons with event listeners and updated game logic

--- script.js
+++ script.js
@@ -1,91 +1,60 @@
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3) + 1;;
+    let choice = Math.floor(Math.random() * 3);
+
+    console.log(choice);
+
     return choice;
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt(`Round ${round}\nEnter your weapon:\nRock, Paper, Scissor`);
-    humanChoice = humanChoice.toLowerCase();
-
-    if (humanChoice === "rock") {
-        choice = 1;
-    } else if (humanChoice === "paper") {
-        choice = 2;
-    } else if (humanChoice === "scissor") {
-        choice = 3;
-    } else {
-        alert("Invalid Choice")
-        playGame()
-    }
-    return choice
-}
-
 function playRound(humanChoice, computerChoice) {
-
-    // Computer Number to Text
-    if (computerChoice === 1) {
-        result = "Rock";
-    } else if (computerChoice === 2) {
-        result = "Paper";
-    } else if (computerChoice === 3) {
-        result = "Scissor";
-    } else {
-        result = "Error Getting Number"
+    choices = {
+        "rock":0,
+        "paper":1,
+        "scissor":2
     }
+    
+    human= choices[humanChoice];
 
-    // Human Number to Text
-    if (humanChoice === 1) {
-        choice = "Rock";
-    } else if (humanChoice === 2) {
-        choice = "Paper";
-    } else if (humanChoice === 3) {
-        choice = "Scissor";
-    }
-
-    if (humanChoice === computerChoice ) {
-        humanScore += 1;
-        computerScore += 1;
-        alert(`Round ${round}:\nComputer: ${result} vs Human: ${choice} \nDraw\nHuman Score: ${humanScore}\nComputer Score: ${computerScore}`);
-    } else if (humanChoice === 1 && computerChoice === 3 || humanChoice === 2 && computerChoice === 1 || humanChoice === 3 && computerChoice === 2) {
-        humanScore += 1;
-        alert(`Round ${round}:\nComputer: ${result} vs Human: ${choice} \nHuman Wins\nHuman Score: ${humanScore}\nComputer Score: ${computerScore}`);
-    } else if (computerChoice === 1 && humanChoice === 3 || computerChoice === 2 && humanChoice === 1 || computerChoice === 3 && humanChoice === 2) {
-        computerScore += 1;
-        alert(`Round ${round}:\nComputer: ${result} vs Human: ${choice} \nComputer Wins\nHuman Score: ${humanScore}\nComputer Score: ${computerScore}`);
-    }
+    result = (human - computer + 3) % 3;
+    return  result
 }
 
-function playGame() {
-    round += 1;
-    let computerChoice= getComputerChoice();
-    let humanChoice = getHumanChoice();
+// function playGame() {
+//     round += 1;
+    
+//     let humanChoice = getHumanChoice();
 
-    playRound(humanChoice, computerChoice);
-}
+//     playRound(humanChoice, computerChoice);
+// }
 
-function results(a, b) {
-    if (a > b) {
-        alert("HUMAN WINS")
-    } else if (a < b) {
-        alert("COMPUTER WINS")
-    }
-}
+// function results(a, b) {
+//     if (a > b) {
+//         alert("HUMAN WINS")
+//     } else if (a < b) {
+//         alert("COMPUTER WINS")
+//     }
+// }
 
-function initialize() {
-    round = 0;
-    while (round < 5) {
-        playGame();
-    }
+// function initialize() {
+//     round = 0;
+//     while (round < 5) {
+//         playGame();
+//     }
 
-    if (round === 5) {
-        results(humanScore, computerScore)
-    }
-}
+//     if (round === 5) {
+//         results(humanScore, computerScore)
+//     }
+// }
 
 let round = 0;
 let humanScore = 0;
 let computerScore = 0;
 
-const start = document.querySelector(".start-btn")
+const buttons = document.querySelectorAll(".btn");
+const score = document.querySelector(".score");
 
-start.addEventListener("click", initialize);
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.id, getComputerChoice());
+    });
+});
