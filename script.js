48a42665345a51aaf7604bcf9ea10b1846dc8f99
johnmichael <91875751+michaelll22@users.noreply.github.com>
2025-09-04 15:35:24 +0800

    Added display for human and computer picks and use flex layout

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    console.log(choice);
+    console.log(`computer: ${choice}`);
     return choice;
 }
 
 function playRound(humanChoice, computerChoice) {
-    choices = {
-        "rock":0,
-        "paper":1,
-        "scissor":2
-    }
- 
-    human= choices[humanChoice];
+
+    choices = ["Rock","Paper","Scissor"]
+    capitalized = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    
+    human = choices.indexOf(capitalized);
+
+    console.log(`human: ${human}`);
+    
 
     const result = (human - computerChoice + 3) % 3;
-    return displayScore(result)
+    return displayScore(result, human, computerChoice);
 }
 
-function displayScore (result) {
+function displayScore (result, human, computer) {
     switch (result) {
         case 0:
-            
+            pick.textContent = `${choices[human]} vs ${choices[computer]}`;
+            roundWinner.textContent = "Draw!";
             break;
+            
         case 1:
+            pick.textContent = `${choices[human]} vs ${choices[computer]}`;
+            roundWinner.textContent = "Human Win this round!";
             humanPoints++;
-            humanScore.textContent = humanPoints;
-            computerScore.textContent = computerPoints;
             break;
         case 2:
+            pick.textContent = `${choices[human]} vs ${choices[computer]}`;
+            roundWinner.textContent = "Computer Win this round!";
             computerPoints++;
-            humanScore.textContent = humanPoints;
-            computerScore.textContent = computerPoints;
             break;
         default:
-            break;
+            break;     
     }
+    humanScore.textContent = humanPoints;
+    computerScore.textContent = computerPoints;
+    scoreContainer.appendChild(pick);
+    scoreContainer.appendChild(roundWinner)
 };
 
 let round = 0;
@@ -44,6 +51,12 @@ let computerPoints = 0;
 const buttons = document.querySelectorAll(".btn");
 const humanScore = document.querySelector(".human-score");
 const computerScore = document.querySelector(".computer-score");
+const scoreContainer = document.querySelector(".container");
+const pick = document.createElement("p");
+const roundWinner = document.createElement("p");
+
+
+roundWinner.classList.add = "round-win";
 
 humanScore.textContent = humanPoints;
 computerScore.textContent = computerPoints;
